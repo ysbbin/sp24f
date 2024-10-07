8e7357fb9f433a46c8fa26f96031a83e79302b71
Eunji Lee <eunjicious@gmail.com>
2024-10-07 14:30:28 +0900

    updated

--- opt_lab/sum.c
+++ opt_lab/sum.c
@@ -3,12 +3,9 @@
 #include<assert.h>
 #include<limits.h>
 
-//#define MAX_NUM 100000000
-#define MAX_NUM 10
-//#define MAX_NUM 1000000000
-//#define MAX_NUM 1000000000
-//#define MAX_NUM INT_MAX
-//#define MAX_NUM 100000
+//#define MAX_NUM 10
+#define MAX_NUM 1000000000
+
 typedef double data_t;
 
 struct vec {
